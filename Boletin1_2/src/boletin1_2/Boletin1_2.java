9377f0d968eb159a25a466996e44a13cc039ca19
finfanterodal <finfanterodal@nautilus11.danielcastelao.org>
2018-10-09 09:19:04 +0200

    Boletin1_2

---
+++ Boletin1_2/src/boletin1_2/Boletin1_2.java
@@ -0,0 +1,22 @@
+
+package boletin1_2;
+
+/**
+ *
+ * @author finfanterodal
+ */
+public class Boletin1_2 {
+
+    public static void main(String[] args) {
+       
+        float lado= 3.0f,acuadrado;
+        acuadrado=lado*lado;
+        System.out.println("Área del cuadrado = "+acuadrado);
+        
+       //Sin crear la variable acuadrado.
+        System.out.println("Área del cuadrado = "+(lado*lado));
+      
+        
+    }
+    
+}
